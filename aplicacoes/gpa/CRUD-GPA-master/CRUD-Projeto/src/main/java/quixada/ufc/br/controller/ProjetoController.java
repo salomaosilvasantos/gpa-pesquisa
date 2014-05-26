5e3f5980d5ef206383d2d67913c7c3b0116e6387
sergiofffilho <sergiofffilho@gmail.com>
2014-05-26 10:56:29 -0300

    Autenticação no login ok. Login "admin", senha "123456". Falta ainda adicionar as permissões.

--- aplicacoes/gpa/CRUD-GPA-master/CRUD-Projeto/src/main/java/quixada/ufc/br/controller/ProjetoController.java
+++ aplicacoes/gpa/CRUD-GPA-master/CRUD-Projeto/src/main/java/quixada/ufc/br/controller/ProjetoController.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +31,9 @@ public class ProjetoController {
 	private static int contador = 0;
 
 	private Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	
+ 
 
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String index() {
